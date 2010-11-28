04d3e18e13e352b2398abbaa9355a457a65991d7
mpwalsh8 <mpwalsh8@c324b110-6d8c-271e-f6ff-5d3822abaacd>
2010-11-28 12:58:34 +0000

    Fixed include path so it will work on Windows and Linux.

--- trunk/webapps/sdif2mysql/htdocs/index.php
+++ trunk/webapps/sdif2mysql/htdocs/index.php
@@ -12,7 +12,8 @@
 
 //make sure the lib dir is in the default include path
 $lib_path = realpath('../lib');
-ini_set('include_path', '.:/usr/local/lib:'.$lib_path);
+//ini_set('include_path', '.:/usr/local/lib:'.$lib_path);
+ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . $lib_path);
 
 //need this for external lib paths includes
 //that happen outside of autoload.
