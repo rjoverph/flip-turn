da6a4ca8f3376965481b5ecafe3346f8280ff90e
mpwalsh8 <mpwalsh8@c324b110-6d8c-271e-f6ff-5d3822abaacd>
2011-01-01 16:18:10 +0000

    Initial checkin.

---
+++ trunk/webapps/sdif2mysql/include/ft.include.php
@@ -0,0 +1,33 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+/**
+ *
+ * $Id$
+ *
+ * FT includes.  These includes define information used in 
+ * acros the Flip-Turn application
+ *
+ * (c) 2010 by Mike Walsh for Flip-Turn
+ *
+ * @author Mike Walsh <mike_walsh@mindspring.com>
+ * @package Flip-Turn
+ * @version $Revision$
+ * @lastmodified $Date$
+ * @lastmodifiedby $Author$
+ *
+ */
+
+define('FT_DEBUG', true) ;
+define('FT_CREATE', 'create') ;
+define('FT_IGNORE', 'ignore') ;
+
+define('FT_NULL_STRING', '') ;
+
+define('FT_ACTION_ADD', 'add') ;
+define('FT_ACTION_UPDATE', 'update') ;
+define('FT_ACTION_DELETE', 'delete') ;
+
+define('FT_US_ONLY', false) ;
+
+?>
+
