d2dc286d5f7da1fb9bc47d14161a1dadc92e7f4a
mpwalsh8 <mpwalsh8@c324b110-6d8c-271e-f6ff-5d3822abaacd>
2011-01-01 16:17:17 +0000

    Cleaned up include path.

--- trunk/webapps/sdif2mysql/index.php
+++ trunk/webapps/sdif2mysql/index.php
@@ -1,1 +1,43 @@
-<?php phpinfo() ; ?>
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+/**
+ * Home Page
+ *
+ * $Id$
+ *
+ * @author Mike Walsh <mike_walsh@mindspring.com>
+ * @package Flip Turn
+ * @subpackage Home
+ *
+ */ 
+
+// * Include the phphtmllib libraries
+include("includes.inc") ;
+
+include("page.class.php") ;
+
+/**
+ * Home stuff
+ *
+ * @author Mike Walsh <mike_walsh@mindspring.com>
+ * @access public
+ * @see FlipTurnLayoutPage
+ *
+ */
+class FlipTurnHomePage extends FlipTurnLayoutPage
+{
+    function content_block()
+    {
+	    $container = container() ;
+
+        $container->add("Home content goes here.") ;
+
+	    return $container ;
+    }
+}
+
+
+$page = new FlipTurnHomePage("Home") ;
+
+print $page->render() ;
+?>
