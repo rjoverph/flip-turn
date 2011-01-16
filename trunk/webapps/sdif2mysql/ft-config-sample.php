61c6110b500e4d8351454e4db44d714d5108db06
mpwalsh8 <mpwalsh8@c324b110-6d8c-271e-f6ff-5d3822abaacd>
2011-01-16 12:43:31 +0000

    Renamed and cleaned up for deployment.

---
+++ trunk/webapps/sdif2mysql/ft-config-sample.php
@@ -0,0 +1,40 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+/**
+ *
+ * $Id$
+ *
+ * FT Config
+ *
+ * (c) 2010 by Mike Walsh for Flip-Turn
+ *
+ * @author Mike Walsh <mike_walsh@mindspring.com>
+ * @package Flip-Turn
+ * @subpackage Config
+ * @version $Revision$
+ * @lastmodified $Date$
+ * @lastmodifiedby $Author$
+ *
+ */
+
+/**
+ * The MySQL username to use
+ */
+define('FT_DB_USERNAME', 'tbd') ;
+
+/**
+ * The MySQL password to use
+ */
+define('FT_DB_PASSWORD', 'tbd') ;
+
+/**
+ * The MySQL database server to use
+ */
+define('FT_DB_HOSTNAME', 'tbd');
+
+/**
+ * The MySQL database name to use
+ */
+define('FT_DB_NAME', 'tbd') ;
+
+?>
