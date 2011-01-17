212217436cd06a5614b98c04c233c8062144152e
mpwalsh8 <mpwalsh8@c324b110-6d8c-271e-f6ff-5d3822abaacd>
2011-01-17 18:40:15 +0000

    Added new constants for the options database.

--- webapps/flip-turn/include/ft.include.php
+++ webapps/flip-turn/include/ft.include.php
@@ -27,9 +27,10 @@ define('FT_ACTION_ADD', 'add') ;
 define('FT_ACTION_UPDATE', 'update') ;
 define('FT_ACTION_DELETE', 'delete') ;
 
-//define('FT_US_ONLY', false) ;
-
 define('FT_LOGIN_STATUS', 'ft_login_status') ;
-//define('FT_PASSWORD', 'SimpleSimon101') ;
 
+define('FT_HOME_PAGE_OPTION', 'ft_home_page_content') ;
+define('FT_ABOUT_PAGE_OPTION', 'ft_about_page_content') ;
+define('FT_LEGAL_PAGE_OPTION', 'ft_legal_page_content') ;
+define('FT_PRIVACY_PAGE_OPTION', 'ft_privacy_page_content') ;
 ?>
